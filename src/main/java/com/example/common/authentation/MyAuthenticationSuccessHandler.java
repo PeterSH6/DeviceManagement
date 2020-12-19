cdeab3b867bc788afa9feaf806384b389402d68d
PeterSheng <petershengwhu@gmail.com>
2020-12-19 23:41:37 +0800

    修改登录

--- src/main/java/com/example/common/authentation/MyAuthenticationSuccessHandler.java
+++ src/main/java/com/example/common/authentation/MyAuthenticationSuccessHandler.java
@@ -1,5 +1,6 @@
 package com.example.common.authentation;
 
+import com.example.cache.UserCache;
 import com.example.common.response.RespBean;
 import com.example.common.response.RespCode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -7,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserCache;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -21,13 +21,13 @@ import java.io.PrintWriter;
 @Slf4j
 public class MyAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
-    //@Autowired
-    //private UserCache userCache;
+    @Autowired
+    private UserCache userCache;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
         log.info("Login success, loading user info from database");
-        //userCache.getUser(authentication.getName());
+        userCache.getUser(authentication.getName());
         httpServletResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
         PrintWriter out = httpServletResponse.getWriter();
 
--- src/main/java/com/example/common/authentation/MyLogoutHandler.java
+++ src/main/java/com/example/common/authentation/MyLogoutHandler.java
@@ -1,6 +1,8 @@
 package com.example.common.authentation;
 
+import com.example.cache.UserCache;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Component;
@@ -12,13 +14,13 @@ import javax.servlet.http.HttpServletResponse;
 @Component
 public class MyLogoutHandler implements LogoutHandler {
 
-    //@Autowire
-    //private UserCache userCache;
+    @Autowired
+    private UserCache userCache;
 
     @Override
     public void logout(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) {
         log.info("Logout success," + authentication.getName() + "logout...");
-        //userCache.putUser(userCache.getUser(authenticaiton.getName());
-        //userCache.evictUser(authentication.getName());
+        userCache.putUser(userCache.getUser(authentication.getName()));
+        userCache.evictUser(userCache.getUser(authentication.getName()));
     }
 }
